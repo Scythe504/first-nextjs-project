648a9ba643e1bb062ebe078c187915a73c210027
Scythe504 <hellfireprince98@gmail.com>
2024-03-06 19:35:12 +0530

    Completed an login form

---
+++ src/components/form-error.tsx
@@ -0,0 +1,20 @@
+import { LucideAlertTriangle} from "lucide-react"
+
+interface FormErrorProps {
+    message?: string,
+}
+
+export const FormError = ({
+    message
+}: FormErrorProps)=>{
+    if(!message) return null 
+
+    return <div 
+    className="bg-red-500/15 p-3 
+    rounded-md flex items-center
+    gap-x-2 text-sm text-destructive
+    ">
+        <LucideAlertTriangle className="h-4 w-4"/>
+        <p>{message}</p>
+    </div>
+}
